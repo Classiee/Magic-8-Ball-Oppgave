4a8d5a9e5e0185b49e13748b888a71cb31535072
Classiee <Kristianfratini@gmail.com>
2024-08-22 13:04:49 +0200

    a bug

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function getRandomAnswer() {
 
 function showAnswer() {
     document.getElementById('answerText').innerHTML = answer;
-    document.getElementById('showAnswer').style.backgroundImage = 'url("8ballpop.png")';
+    document.getElementById('showAnswer').style.backgroundImage = 'url("popup.png")';
     document.getElementById('showAnswer').style.display = 'block';
     document.querySelector('.overlay').style.display = 'block';
 }
